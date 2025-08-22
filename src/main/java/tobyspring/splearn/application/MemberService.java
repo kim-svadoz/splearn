545c924d011df8331686281320cb8ea191f37111
kim-svadoz <dhkdghehfdl@gmail.com>
2025-08-22 23:06:06 +0900

    feat: 애플리케이션 서비스 테스트 작성

---
+++ src/main/java/tobyspring/splearn/application/MemberService.java
@@ -0,0 +1,48 @@
+package tobyspring.splearn.application;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import tobyspring.splearn.application.provided.MemberRegister;
+import tobyspring.splearn.application.required.EmailSender;
+import tobyspring.splearn.application.required.MemberRepository;
+import tobyspring.splearn.domain.DuplicateEmailException;
+import tobyspring.splearn.domain.Email;
+import tobyspring.splearn.domain.Member;
+import tobyspring.splearn.domain.MemberRegisterRequest;
+import tobyspring.splearn.domain.PasswordEncoder;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements MemberRegister {
+    private final MemberRepository memberRepository;
+    private final EmailSender emailSender;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Member register(MemberRegisterRequest registerRequest) {
+        // check
+        checkDuplicateEmail(registerRequest);
+
+        // domain model
+        Member member = Member.register(registerRequest, passwordEncoder);
+
+        // repository
+        memberRepository.save(member);
+
+        // post process
+        sendWelcomeEmail(member);
+
+        return member;
+    }
+
+    private void sendWelcomeEmail(Member member) {
+        emailSender.send(member.getEmail(), "등록을 완료해주세요", "아래 ㄹ이크를 클릭해서 등록을 완료해주세요");
+    }
+
+    private void checkDuplicateEmail(MemberRegisterRequest registerRequest) {
+        if (memberRepository.findByEmail(new Email(registerRequest.email())).isPresent()) {
+            throw new DuplicateEmailException("이미 사용중인 이메일입니다: " + registerRequest.email());
+        }
+    }
+}
---
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -0,0 +1,39 @@
+package tobyspring.splearn.application.provided;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestConstructor;
+
+import jakarta.transaction.Transactional;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import tobyspring.splearn.SplearnTestConfiguration;
+import tobyspring.splearn.domain.DuplicateEmailException;
+import tobyspring.splearn.domain.Member;
+import tobyspring.splearn.domain.MemberFixture;
+import tobyspring.splearn.domain.MemberStatus;
+
+@SpringBootTest
+@Transactional
+@Import(SplearnTestConfiguration.class)
+// @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public record MemberRegisterTest(MemberRegister memberRegister) {
+
+    @Test
+    void register() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThat(member.getId()).isNotNull();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThatThrownBy(() -> memberRegister.register(MemberFixture.createMemberRegisterRequest()))
+            .isInstanceOf(DuplicateEmailException.class);
+    }
+
+}
